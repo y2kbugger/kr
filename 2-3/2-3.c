c207d885f62bdb90c3987b12701078d5ee056722
y2kbugger <git@y2kbugger.com>
2019-04-04 22:27:32 -0400

    begin 2-3

---
+++ 2-3/2-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+
+/*
+ * Exercise 2-3 write htoi(s) convert hex to int
+ */
+
+int htoi(char s[])
+{
+    return 255;
+}
+
+int main()
+{
+    /* int lim=99; */
+    /* char s[lim+1]; */
+    /* strcpy(s, "0xFF"); //255 */
+    /* int c; */
+    /* c = htoi(s); */
+    printf("%s:%x\n", "FF", htoi("FF"));
+}
