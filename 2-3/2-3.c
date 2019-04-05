ab813376d98618449db6f8439cb4c0838bc8206e
y2kbugger <git@y2kbugger.com>
2019-04-04 22:51:39 -0400

    works with single digit numerics

--- 2-3/2-3.c
+++ 2-3/2-3.c
@@ -8,7 +8,15 @@
 
 int htoi(char s[])
 {
-    return 255;
+    long value = 0;
+    char *c;
+    for (c = s; *c !='\0'; c++)
+    {
+        value += *c - 48;
+        putchar(*c);
+        putchar('\n');
+    }
+    return value;
 }
 
 int main()
@@ -18,5 +26,9 @@ int main()
     /* strcpy(s, "0xFF"); //255 */
     /* int c; */
     /* c = htoi(s); */
+    printf("%s:%x\n", "1", htoi("1"));
+    printf("%s:%x\n", "2", htoi("2"));
+    printf("%s:%x\n", "3", htoi("3"));
+    printf("%s:%x\n", "333", htoi("333"));
     printf("%s:%x\n", "FF", htoi("FF"));
 }
