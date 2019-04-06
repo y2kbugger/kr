e7bf978a9ac99b6cc653a0babd47b5344b0f2625
y2kbugger <git@y2kbugger.com>
2019-04-05 20:44:59 -0400

    finished 2-3

--- 2-3/2-3.c
+++ 2-3/2-3.c
@@ -11,14 +11,33 @@ int htoi(char s[])
     unsigned long value = 0;
     unsigned int digitval, place;
     char *c;
+
+    // remove optional leading 0x
+    if (s[0] == '0')
+        if (s[1] == 'X' || s[1] == 'x')
+            s += 2;
+
+    // find the end of the string
     for (c = s; *c != '\0'; c++){}
     char *onesplace = c;
+
+    // loop over each digit, summing the total, weighted by 16^0 for the lsd, then 16^1, 16^2, etc.
     for (c=c-1;  c>=s ; c--)
     {
         place=onesplace-c-1;
-        digitval = *c - 48;
+        if ('0'<=*c && *c<='9')
+            digitval = *c - '0';
+        else if ('a'<=*c && *c<='f')
+            digitval = *c - 'a'+10;
+        else if ('A'<=*c && *c<='F')
+            digitval = *c - 'A'+10;
+        else
+            return -1;
+
+        // do weighting
         value += (digitval * (1 << 4*place));
 
+        // debug each digit
         printf("%d|\t%c:%x\n",place,*c, digitval);
     }
     return value;
@@ -26,12 +45,6 @@ int htoi(char s[])
 
 int main()
 {
-    /* int lim=99; */
-    /* char s[lim+1]; */
-    /* strcpy(s, "0xFF"); //255 */
-    /* int c; */
-    /* c = htoi(s); */
-    printf("%d\n", 16<<1);
     printf("\t\t%s:%x\n\n", "1", htoi("1"));
     printf("\t\t%s:%x\n\n", "2", htoi("2"));
     printf("\t\t%s:%x\n\n", "2", htoi("2"));
@@ -39,4 +52,8 @@ int main()
     printf("\t\t%s:%x\n\n", "3", htoi("3"));
     printf("\t\t%s:%x\n\n", "333", htoi("333"));
     printf("\t\t%s:%x\n\n", "FF", htoi("FF"));
+    printf("\t\t%s:%x\n\n", "ff", htoi("ff"));
+    printf("\t\t%s:%x\n\n", "0xff", htoi("0xff"));
+    printf("\t\t%s:%x\n\n", "0Xff", htoi("0Xff"));
+    printf("\t\t%s:%x\n\n", "fqf", htoi("fqf"));
 }
