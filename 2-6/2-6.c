e6ad46996152d7d32a325c24614b762995b801c4
y2kbugger <git@y2kbugger.com>
2019-04-09 13:48:17 -0400

    Complete 2-6

--- 2-6/2-6.c
+++ 2-6/2-6.c
@@ -6,32 +6,41 @@
  * Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged.
  */
 
-int setbits(int x, int p, int n, int y)
+unsigned int setbits(unsigned int x, int p, int n, int y)
 {
     // x: 10101010
     // p: 3
     // n: 2
     // y: 00000010
+
     // move the bits of y to the correct location
-    //    01234567
-    // y: 00010000 // y = y << width - p
-    //
+    //    76543210
+    // y: 00001000
+    y = y << (p+1-n);
     // make a bitmask
-    //    00011000
+    //    00001100
     // clear bits of x
+    unsigned m;
+    m = ~0;
+    // m: 11111111
+    m = m << (p+1 - n);
+    // m: 11111100
+    m = ~m;
+    // m: 00000011
+    m = m << n;
+    // m: 00001100
+    return (x&~m)|(y&m);
+
     // add bits of the shifted y
 }
 
 void testit(char s1[], char s2[])
 {
-    printf("%s|%s:  %d\n", s1, s2, any(s1, s2));
+    printf("%d\n", setbits(0, 3, 2, 0777));
+    printf("%d\n", setbits(07777, 3, 2, 0));
 }
 
 int main()
 {
     testit("hello", "oh");
-    testit("worldhelloit is time to get up", "zxfsz");
-    testit("worldhelloit is time to get up", "z");
-    testit("worldhelloit is time to get up", "");
-    testit("worldhelloit is time to get up", "elz");
 }
