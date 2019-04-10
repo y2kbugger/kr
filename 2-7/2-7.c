d3accddcb378d5b9af6205fb9fa5b593dae8a82c
y2kbugger <git@y2kbugger.com>
2019-04-10 00:02:30 -0400

    Complete 2-7

--- 2-7/2-7.c
+++ 2-7/2-7.c
@@ -6,32 +6,50 @@
  * Exercise 2-7. Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.
  */
 
-int setbits(int x, int p, int n, int y)
+uint16_t invert(uint16_t x, int p, int n)
 {
     // x: 10101010
     // p: 3
     // n: 2
-    // y: 00000010
-    // move the bits of y to the correct location
-    //    01234567
-    // y: 00010000 // y = y << width - p
-    //
+    //    76543210
     // make a bitmask
     //    00011000
-    // clear bits of x
-    // add bits of the shifted y
+    uint16_t m;
+    m = ~((~0 << n)) << (p+1-n);
+    uint16_t y, xx;
+    y = ~x & m;
+    x = x & ~m;
+    return y|x;
+
+}
+const char *bit_rep[16] = {
+    [ 0] = "0000", [ 1] = "0001", [ 2] = "0010", [ 3] = "0011",
+    [ 4] = "0100", [ 5] = "0101", [ 6] = "0110", [ 7] = "0111",
+    [ 8] = "1000", [ 9] = "1001", [10] = "1010", [11] = "1011",
+    [12] = "1100", [13] = "1101", [14] = "1110", [15] = "1111",
+};
+
+void print_binary(uint16_t byte)
+{
+    printf("%s%s%s%s",
+        bit_rep[(byte & 0xF000) >> 12],
+        bit_rep[(byte & 0x0F00) >>  8],
+        bit_rep[(byte & 0x00F0) >>  4],
+        bit_rep[(byte & 0x000F) >>  0]
+        );
 }
 
-void testit(char s1[], char s2[])
+void testit(uint16_t x, int p, int n)
 {
-    printf("%s|%s:  %d\n", s1, s2, any(s1, s2));
+
+    print_binary(x);
+    printf(";%d;%d: ", p,n);
+    print_binary(invert(x,p,n));
+    printf("\n");
 }
 
 int main()
 {
-    testit("hello", "oh");
-    testit("worldhelloit is time to get up", "zxfsz");
-    testit("worldhelloit is time to get up", "z");
-    testit("worldhelloit is time to get up", "");
-    testit("worldhelloit is time to get up", "elz");
+    testit(077777, 3, 2);
+    testit(077777, 6, 2);
 }
