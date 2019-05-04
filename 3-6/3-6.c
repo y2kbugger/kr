ad0fb3819dce112a3af0ca83207bdd49449f216e
y2kbugger <git@y2kbugger.com>
2019-05-03 20:35:17 -0400

    Fix problem number

--- 3-6/3-6.c
+++ 3-6/3-6.c
@@ -9,7 +9,7 @@
 #define TESTREPS 1
 
 /*
- * Exercise 3-4. Write a version of itoa that accepts three arguments instead of two. The third argument is a minimum field width; the converted number must be padded with blanks on the left if necessary to make it wide enough.
+ * Exercise 3-6. Write a version of itoa that accepts three arguments instead of two. The third argument is a minimum field width; the converted number must be padded with blanks on the left if necessary to make it wide enough.
  *
  * pad with zeros after numeric sign(if neg), inckude it as part of the
  * field width.
