7e9363717b30d106c6584fcc9d92488f738b5c13
y2k <git@y2kbugger.com>
2019-05-12 23:04:46 -0400

    Start outline of 4-3

--- 4-3/4-3.c
+++ 4-3/4-3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
-#include <stdlib.h>
+#include <stdlib.h> /* for atof() */
 #include <stdint.h>
 #include <time.h>
 #include <stdbool.h>
@@ -46,14 +46,47 @@ void reverse(char s[])
     }
 }
 
-/* atof: convert string to float */
-double atof2(char s[])
-{
-#include <stdio.h>
-#include <stdlib.h> /* for atof() */
 #define MAXOP 100 /* max size of operand or operator */
-#define NUMBER ′0′ /* signal that a number was found */
-    int getop(char []); void push(double); double pop(void);  /* reverse Polish calculator */ main() {  int type;  double op2;  char s[MAXOP];  while ((type = getop(s)) != EOF) {  switch (type) {  case NUMBER:  push(atof(s));  break;  case ′+′:  push(pop() + pop());  break;  case ′*′:  push(pop() * pop());  break;  case ′-′:  op2 = pop();  push(pop() - op2);  break;  case ′/′:  op2 = pop();  if (op2 != 0.0)  push(pop() / op2);  else  printf("error: zero divisor\n");  break;  case ′\n′:  printf("\t%.8g\n", pop());  break;  default:  printf("error: unknown command %s\n", s);  break;  }  }  return 0; }
+#define NUMBER '0' /* signal that a number was found */
+int getop(char []);
+void push(double);
+double pop(void);
+
+/* reverse Polish calculator */
+main() {
+    int type;
+    double op2;
+    char s[MAXOP];
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+ '':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else  printf("error: zero divisor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+    return 0;
 }
 
 
