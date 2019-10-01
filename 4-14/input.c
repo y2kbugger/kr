a8c6c513da7c45ad58c4940b139ddedf41b8a649
y2k <git@y2kbugger.com>
2019-10-01 16:52:31 -0400

    Finish 4-14

--- 4-14/input.c
+++ 4-14/input.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
-
 /*
  * Exercise 4-14. Define a macro swap(t,x,y) that interchanges two arguments of
  * type t. (Block structure will help.)
  */
 
-
-void testit()
-{
-    swap(int, 4, 5);
-}
+#define swap(t,x,y) { t z; z = x; x = y; y = z; }
 
 int main()
 {
-    testit();
+    int a = 2;
+    int b = 7;
+    printf("a:%d, b:%d\n", a, b);
+    swap(int, a, b);
+    printf("a:%d, b:%d\n", a, b);
+    swap(int, a, b);
+    printf("a:%d, b:%d\n", a, b);
+    /* printf("z:%d", z); */// I was double checking that the curlys kept the z out of scope
+
 }
