df63d2699dc0aaf71abecc45845cf4a45d328fd3
y2k <git@y2kbugger.com>
2019-10-14 20:57:31 -0400

    Finish 5-4

--- 5-4/input.c
+++ 5-4/input.c
@@ -6,11 +6,37 @@
  * the string t occurs at the end of the string s, and zero otherwise.
  */
 
-
-/* strcat: concatenate t to end of s; s must be big enough */
-
 int strend(char *s, char *t)
 {
+    char *s_init = s;
+    char *t_init = t;
+
+    /* find end of strings */
+    while (*++s);
+    while (*++t);
+
+    printf("\n");
+    /* compare t to end of s */
+    while (1) {
+        --t, --s;
+
+        printf("\t\t%c:%c\n", *s, *t);
+
+        if (*s != *t) {
+            printf("There was a real char mismatch\n");
+            return 0;
+        }
+
+        if (t == t_init)
+            return 1;
+
+        /* s ended before t */
+        if (s == s_init) {
+            printf("s ran out of chars\n");
+            return 0;
+        }
+
+    }
     return 1;
 }
 
@@ -18,7 +44,7 @@ void testit(char *s, char *t, int expected)
 {
     printf("%s;%s:", s, t);
     int actual = strend(s, t);
-    printf("|%d:%d\n", actual, expected);
+    printf("\t\t\t%d:%d\n\n\n", actual, expected);
 }
 
 
@@ -26,9 +52,14 @@ int main()
 {
     testit("", "a", 0);
     testit("a", "", 0);
+    testit("a", "ab", 0);
+    testit("ab", "a", 0);
     testit("one", "two", 0);
     testit("taco", "bell", 0);
     testit("tacobell", "nachosbellgrande", 0);
     testit("tacobell", "bell", 1);
     testit("tacobell", "bella", 0);
+    testit("bell", "bell", 1);
+    testit("bell", "belb", 0);
+    testit("bell", "abell", 0);
 }
