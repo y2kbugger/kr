cb69b2e0e23af8d2fe0fc10f2627f773d230effc
y2k <git@y2kbugger.com>
2019-11-03 22:44:08 -0500

    WIP

--- 5-7/input.c
+++ 5-7/input.c
@@ -5,11 +5,17 @@
  * Exercise 5-7. Rewrite readlines to store lines in an array supplied by main,
  * rather than calling alloc to maintain storage. How much faster is the
  * program
+
+$ time <words_alpha.shuffled.txt ./out >fil3
+Original
+real    0m0.520s
+
+After
  */
 
-#define MAXLINES 5000           /* max #lines to be sorted  */
+#define MAXLINES 500000         /* max #lines to be sorted  */
 
-#define ALLOCSIZE 100000        /* size of available space  */
+#define ALLOCSIZE 10000000      /* size of available space  */
 static char allocbuf[ALLOCSIZE];        /* storage for alloc */
 static char *allocp = allocbuf; /* next free position */
 
@@ -53,18 +59,20 @@ void writelines(char *lineptr[], int nlines)
 
 /* ========================= */
 
+#define MAXLEN 100
 char *lineptr[MAXLINES];        /* pointers to text lines  */
 
-int readlines(char *lineptr[], int nlines);
+int readlines(char *lineptr[], int nlines, char *linestore);
 void writelines(char *lineptr[], int nlines);
 void _qsort(char *lineptr[], int left, int right);
 
 /* sort input lines  */
 int main()
 {
-    /* number of input lines read  */
-    int nlines;
-    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    int nlines;                 /* number of input lines read  */
+    char *linestore = malloc(MAXLINES * MAXLEN);        /* storage for lines */
+
+    if ((nlines = readlines(lineptr, MAXLINES, linestore)) >= 0) {
         _qsort(lineptr, 0, nlines - 1);
         writelines(lineptr, nlines);
         return 0;
@@ -74,25 +82,23 @@ int main()
     }
 }
 
-#define MAXLEN 1000
 /* max length of any input line */
 int _getline(char s[], int);
 char *alloc(int);
 
 /* readlines:  read input lines */
-int readlines(char *lineptr[], int maxlines)
+int readlines(char *lineptr[], int maxlines, char *linestore)
 {
     int len, nlines;
-    char *p, line[MAXLEN];
 
     nlines = 0;
-    while ((len = _getline(line, MAXLEN)) > 0)
-        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+    while ((len = _getline(linestore, MAXLEN)) > 0)
+        if (nlines >= maxlines)
             return -1;
         else {
-            line[len - 1] = '\0';       /* delete newline  */
-            strcpy(p, line);
-            lineptr[nlines++] = p;
+            linestore[len - 1] = '\0';  /* delete newline  */
+            linestore += len;   /* move to next availble slot */
+            lineptr[nlines++] = linestore;;
         }
     return nlines;
 }
@@ -102,7 +108,6 @@ void _qsort(char *v[], int left, int right)
 {
     int i, last;
     void swap(char *v[], int i, int j);
-
     if (left >= right)          /* do nothing if array contains */
         return;                 /* fewer than two elements */
     swap(v, left, (left + right) / 2);
@@ -119,7 +124,6 @@ void _qsort(char *v[], int left, int right)
 void swap(char *v[], int i, int j)
 {
     char *temp;
-
     temp = v[i];
     v[i] = v[j];
     v[j] = temp;
