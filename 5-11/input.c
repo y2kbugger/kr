89fb561b42335c9ec8b0d768187c5a0ef0078356
y2k <git@y2kbugger.com>
2019-11-27 05:50:11 -0500

    Complete 5-11

--- 5-11/input.c
+++ 5-11/input.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
-/* Exercise 1-21. Write a program entab that replaces strings of blanks by the minimum number of tabs and blanks to achieve the same spacing. Use the same tab stops as for detab. When either a tab or a single blank would suffice to reach a tab stop, which should be given preference? */
+/* Exercise 5-11: Modify the programs entab and detab (written as exercises in Chapter 1) to accept a list of tab stops as arguments. Use the default tab settings if there are no arguments. */
 
 #define MAXLINE 222
 char TAB = '#';
