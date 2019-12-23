6a6a7fb9687584636bccc85359a13d59be53135e
y2k <git@y2kbugger.com>
2019-12-23 00:45:25 -0500

    Clean up 5-17

--- 5-17/input.c
+++ 5-17/input.c
@@ -124,7 +124,6 @@ int main(int argc, char *argv[])
             printf("unknown arg: '%s'", *argv);
     }
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-        /* myqsort((void **) lineptr, 0, nlines - 1, cmp); */
         mergeSort((void **) lineptr, 0, nlines - 1, cmp);
         writelines(lineptr, nlines);
         return 0;
@@ -199,24 +198,6 @@ void mergeSort(void *v[], int l, int r, Comp comp)
 
         merge(v, l, m, r, comp);
     }
-}
-
-/* myqsort:  sort v[left]...v[right] into increasing order */
-void myqsort(void *v[], int left, int right, Comp comp)
-{
-    int i, last;
-    void swap(void *v[], int, int);
-
-    if (left >= right)          /* do nothing if array contains */
-        return;                 /* fewer than two elements */
-    swap(v, left, (left + right) / 2);
-    last = left;
-    for (i = left + 1; i <= right; i++)
-        if ((*comp) (v[i], v[left]) < 0)
-            swap(v, ++last, i);
-    swap(v, left, last);
-    myqsort(v, left, last - 1, comp);
-    myqsort(v, last + 1, right, comp);
 }
 
 /* return mutated strings before cmp*/
