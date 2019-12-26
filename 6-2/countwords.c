c77f1b1f28ffed78ba53504086e5c99d39c7719b
y2k <git@y2kbugger.com>
2019-12-26 11:23:14 -0500

    Outline 6-2

---
+++ 6-2/countwords.c
@@ -0,0 +1,12 @@
+/* Exercise 6-2. Write a program that reads a C program and prints in
+ * alphabetical order each group of variable names that are identical in the
+ * first 6 characters, but different somewhere thereafter. Don’t count words
+ * within strings and comments. Make 6 a parameter that can be set from the
+ * command line. */
+
+#include <stdlib.h>
+
+int main()
+{
+    exit(0);
+}
