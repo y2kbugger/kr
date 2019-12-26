b3a5312bb658ed174297c678844e7ff3d39296ea
y2k <git@y2kbugger.com>
2019-12-26 15:31:19 -0500

    WIP scaffold out approach.

--- 6-2/countwords.c
+++ 6-2/countwords.c
@@ -6,26 +6,48 @@
  *
  * We need to avoid comments first.
  * Then detect "variable names" via declaration
- * 
+ *
  * Question:
  *  Do we group them in data structure, or only as we print them?
+ * A:
+ *  I think we can store them all and separate into groups while printing
  *
- *  What do they mean by "groups?"
+ * What do they mean by "groups?"
  *
- *  hello_:
- *     hello
+ *   hello
  *
- *  goodby:
- *      goodbye
- *      goodbyeworld
+ *   goodbye
+ *   goodbyeworld
  *
- *  zebraf:
- *      zebrafoot
+ *   zebrafoot
  * */
 
 #include <stdlib.h>
 
 int main()
 {
+    /* parse args */
+    /* -t : print test words */
+    /* -n : number of characters to consider a group */
+
+    /* read in words from stdin */
+    /* get pointer one word at a time, stored in dynamically allocated memory */
+    /* should only return words that are "variable names" (how to detect by context?) */
+    /* search tree for word, if not existing, store the word */
+    /* otherwise free the memory */
+
+
+    /* print out the "variable names" in groups */
+
     exit(0);
+}
+
+print_words(*words)
+{
+    ;
+}
+
+get_word()
+{
+    ;
 }
