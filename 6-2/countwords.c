190e03b136d231814635f109769092be1f92ff27
y2k <git@y2kbugger.com>
2019-12-26 11:37:10 -0500

    WIP Brainstorming interpretation

--- 6-2/countwords.c
+++ 6-2/countwords.c
@@ -2,7 +2,26 @@
  * alphabetical order each group of variable names that are identical in the
  * first 6 characters, but different somewhere thereafter. Don’t count words
  * within strings and comments. Make 6 a parameter that can be set from the
- * command line. */
+ * command line.
+ *
+ * We need to avoid comments first.
+ * Then detect "variable names" via declaration
+ * 
+ * Question:
+ *  Do we group them in data structure, or only as we print them?
+ *
+ *  What do they mean by "groups?"
+ *
+ *  hello_:
+ *     hello
+ *
+ *  goodby:
+ *      goodbye
+ *      goodbyeworld
+ *
+ *  zebraf:
+ *      zebrafoot
+ * */
 
 #include <stdlib.h>
 
