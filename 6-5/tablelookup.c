801e2d05350b588f33ba24ed03a9e960c693ca24
y2k <git@y2kbugger.com>
2019-12-28 11:57:17 -0500

    Add signature for undef, body start out same as install

--- 6-5/tablelookup.c
+++ 6-5/tablelookup.c
@@ -61,6 +61,28 @@ struct nlist *lookup(char *s)
     return NULL;                /* not found */
 }
 
+/* undef:  remove name from hashtab */
+void undef(char *name)
+{
+    struct nlist *np;
+    unsigned hashval;
+
+    if ((np = lookup(name)) == NULL) {  /* not found */
+        np = (struct nlist *) malloc(sizeof(*np));
+        if (np == NULL || (np->name = strdup(name)) == NULL)
+            return NULL;
+        hashval = hash(name);
+        np->next = hashtab[hashval];
+        hashtab[hashval] = np;
+    } else                      /* already there */
+        free((void *) np->defn);        /* free previous defn */
+    if ((np->defn = strdup(defn)) == NULL)
+        return NULL;
+
+    printf("Installed `%s` ==> `%s`\n", name, defn);
+    return np;
+}
+
 /* install:  put (name, defn) in hashtab */
 struct nlist *install(char *name, char *defn)
 {
