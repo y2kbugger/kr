d18a9c98136c177c08c503475fcf11ade41aa5c8
y2k <git@y2kbugger.com>
2019-12-29 14:39:08 -0500

    Outline 7-2

---
+++ 7-2/escape.c
@@ -0,0 +1,10 @@
+/* Exercise 7-2. Write a program that will print arbitrary input in a sensible
+ * way. As a minimum, it should print non-graphic characters in octal or
+ * hexadecimal according to local custom, and break long text lines. */
+
+#include <stdlib.h>
+
+int main()
+{
+    exit(0);
+}
