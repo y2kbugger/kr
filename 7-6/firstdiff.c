8ea2ac8ab50ec28b1264545827c0ec5ab21831af
y2k <git@y2kbugger.com>
2019-12-29 23:42:04 -0500

    Outline 7-6

---
+++ 7-6/firstdiff.c
@@ -0,0 +1,11 @@
+/* Exercise 7-6. Write a program to compare two files, printing the first line
+ * where they differ. */
+
+/* getting this from argv is left as exercise; */
+char FILE1[] = "./file1.txt";
+char FILE2[] = "./file2.txt";
+
+int main()
+{
+
+}
