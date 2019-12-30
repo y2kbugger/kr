3e60d145376c699cec09954cc6c5311f872a2e4c
y2k <git@y2kbugger.com>
2019-12-30 00:24:30 -0500

    Do 7-8 via summary

---
+++ 7-8/input.c
@@ -0,0 +1,7 @@
+/* Exercise 7-8. Write a program to print a set of files, starting each new one
+ * on a new page, with a title and a running page count for each file. */
+
+Get the list of filenames from argv open files with fopen()
+one at a time count lines used to
+do
+    pagebreaks and formats
